b112aa0d6f4e560065d0cead1107efdbb494f016
Miguel Angel <leonaguirrem3@gmail.com>
2019-12-02 16:03:42 -0600

    feat: Asignacion de un ciclo while donde realizan las operaciones para la busqueda

--- Ejercicio 6.9 BusquedaBinariaDeUnNumero.py
+++ Ejercicio 6.9 BusquedaBinariaDeUnNumero.py
@@ -1,3 +1,25 @@
+import math
+
 class BusquedaBinaria:
 
     def Busqueda(self, lista, clave, izquierda, derecha):
+
+        while izquierda <= derecha:
+            # el punto medio del segmento
+            medio = math.floor((izquierda + derecha) / 2)
+            # si el medio es igual al valor buscado, lo devuelve
+            if lista[medio] == clave:
+                return medio
+            # si el valor del punto medio es menor que clave, sigue buscando
+            # en el segmento de la derecha: [medio+1, der],
+            # descartando la izquierda
+            elif lista[medio] < clave:
+                izquierda = medio + 1
+            # sino, sigue buscando en el segmento de la izquierda: [izq, medio-1],
+            # descartando la derecha
+            else:
+                derecha = medio - 1
+            # si no salió del ciclo, vuelve a iterar con el nuevo segmento
+
+            # salió del ciclo de manera no exitosa: el valor no fue encontrado
+        return -1
